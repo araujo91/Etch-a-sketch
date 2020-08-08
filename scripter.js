83e5339ebdb129fb62ac5b77f0a4f8689f8650c6
araujo91 <joaocsaraujo.91@gmail.com>
2020-08-08 17:29:34 +0100

    las tweek: - upon reset black and white - ettche a scketc frame

--- scripter.js
+++ scripter.js
@@ -4,9 +4,7 @@ function divisor(nSquares) {
     for (var i = 0; i < (nSquares*nSquares); i++) {
         const obj = document.createElement('div');
         Object.assign(obj.style,{float:"left",width: size + "px",height: size + "px"});
-        /*obj.style.float = 'left';
-        obj.style.width = size;
-        obj.style.height = size;*/
+        
         obj.classList.add("sand")
         document.getElementById('container').appendChild(obj)
         if (i % nSquares == 0) {
@@ -22,12 +20,50 @@ function reset() {
         myNode.removeChild(myNode.lastElementChild)
     }
     divisor(nSquares);
+    blackWhite();
 }
 
-// hover function 
-/*function hover() {
-    let container = document.getElementById('container')
+function blackWhite() {
+    let container = document.getElementById('container');
     container.addEventListener('mouseover', function(e) {
-          e.target.className += 'permahover';
+    if (e.target.className ==  "sand") {
+        e.target.className += " sandhover"
+    } else {
+        e.target.className = "sand"
+            }
     })
-}*/
+}
+
+function randomHue() {
+    return Math.floor(Math.random() * 360);
+}
+  
+function color() {
+    let container = document.getElementById('container')
+    let hue = randomHue();
+    let light = 50;
+    container.addEventListener('mouseover', function(e) {
+       if (light <= 10) {
+            hue = randomHue();
+            light = 50;
+    } else {
+        light = light * 0.9;
+    }
+        
+        
+        let def = "hsl(" + hue + ", 50%, " + light + "%)";
+        console.log("2º")
+        console.log(def);
+        console.log(typeof(def)) 
+        e.target.style.backgroundColor = def;                
+                })
+}
+
+function rand() {
+    let container = document.getElementById('container')   
+    container.addEventListener('mouseover', function(e) {
+        let hue = randomHue();      
+        let def = "hsl(" + hue + ", 50%, 50%)";
+        e.target.style.backgroundColor = def;                
+        })
+}
